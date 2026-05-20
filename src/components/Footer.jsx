71592a599e0c0a731d70f96941ee5b9a1aeec619
Kwame Dawson <dawsonkwame9@gmail.com>
2026-05-20 13:21:01 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='border-t border-white/5 bg-(--surface-bg) px-5 py-8 text-center text-sm font-medium text-(--text-color) text-sm md:text-base'>
+    <footer className='border-t border-white/5 bg-(--surface-bg) px-5 py-8 text-center text-sm font-medium text-(--text-color) md:text-base'>
       <p>Copyright {new Date().getFullYear()} Kwame Dawson</p>
     </footer>
   )
