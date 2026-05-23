7d9b14978f0d8c4a7ac8fd1908eaba9dec96b802
Kwame Dawson <dawsonkwame9@gmail.com>
2026-05-23 16:31:32 +0000

    updated web analysis package

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
